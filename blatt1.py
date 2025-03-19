8ccea29571ee4b8bf6818bbb32f2d21bf374417e
Florian Reyzek <florian.reyzek@tuwien.ac.at>
2025-03-19 14:49:37 +0100

    Delete blatt2.py, blatt3.py, and blatt4.py
    Removed all content from blatt2.py, blatt3.py, and blatt4.py, effectively deleting these files. Ensure these files are no longer needed to avoid accidental data loss.

--- blatt1.py
+++ blatt1.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# GPA 2024
+# GPA 2025
+
 
 # -------------
 # Aufgabe 1
@@ -7,170 +8,155 @@
 
 # Beispiele aus der Angabe
 
-3 + 4.0  # Addition zweier Zahlen, 7.0, float
+2 + 2 * 3.0  # Multiplikation von 2 mit 3.0 gefolgt von Addition mit 2; 8.0; float
 
-'Hello' * 2  # String vervielfältigen, 'HelloHello', str (String)
+'Hello' * 2  # String mit sich selbst verketten, 'HelloHello', str (String)
 
 # Ihre Lösungen ab hier:
 
-2 + 2 * 3       # Multiplikation und anschließende Addition, 8, int (integer)
+257 - 2 ** 8    # 257 abzgl. 8-faches Potenzieren von 2 -> 1, int
 
-10 / 10 // 2    # Division mit anschließender modulo Operation (Division mit Rest), 0.0, float weil modulo 1/2
+4 * 2.5     # Multiplikation von 4 mit 2.5 9.0 -> 10.0, float
 
-7 / (3 % 2)     # Klammer - Rest von ganzzahliger Division -> 1, int, anschließend division -> gibt immer float aus, 7.0
+'1' + '2.0'     # Addition von strings -> 12.0, str
 
-'0' + '1' * 4   # multiplikation und addition von '' = string, str, 01111
+20 // (7 % 4)   # Division von 20 ohne Rest durch Rest von (7/4) -> int, weil '//' immer int
 
-'ich' + 'und?'[0:3] + 'und?'[-2::-2]
-# Addition von str, Intervall [x:y:z], x start (str beginnt bei 0, -1 letztes zeichen), y ende (nicht mehr dabei), z Schrittweite (wenn negativ von re nach li)
-# ichunddu
+float('123456789'[2])   # Konvertiert index '2' aus dem string in einen float -> 3, float
 
-'Reittier&otto'[1:0:-1]
-# Intervall von str, e
+'Donau Dampfschiff'[6:] + 'Donau Dampfschiff'[:5] # Schneiden des Strings ab index 6 bis zum Ende ':', plus die Zeichen des zweiten strings bis exkl 5. index -> Output str
 
-True and 0  # a and b - Wertet a auf False aus, dann wird a zurückgegeben, Sonst wird b ausgewertet und der Wert von b zurückgegeben
-# Wahrheitswert and 0, a ist True, also wird b ausgegeben,
-# 0, int
+4 > 3 and 8 > 7     # (bool value True, weil 4>3 wahr ist) logisches und (True, weil 8>7), da beide True -> True, bool
 
+4 > (3 and 8) > 7   # (3 and 8) gibt nur int der zweiten Zahl, somit 4 > 8 > 7, weil 8 > 7 False --> False, bool
 
-bool('0' and 'a' in 'Hallo!')
-# bool gibt True oder False aus, 0 = False, alles andere True, hier str, str immer True, wenn nicht anders definiert
-# and vernküpft str '0' und str 'a' beide sind True, weil str 'Hallo!' auch True, Ausgabe True
+'b' and 'a' in 'gpa' # 'b' and 'a' is not a working operation, hence only checks if str'a' in str'gpa' -> True, bool
 
-
-(4 > 2) + bool(3)
-# (...) ergibt True, bool(3) ergibt True, True + True = 2, int
+'Madam'[0::-1]  # Takes the first character of the string from index 0 to end (:), with reverse steps - and a step size 1 -> m, str
 
 
 # -------------
 # Aufgabe 2
 # -------------
 print('\n\n############### Aufgabe 2')
-# TODO: Lösung implementieren
+l_cm = 455  # Länge des Zimmers in cm
+b_cm = 290  # Länge des Zimmers in cm
+h_cm = 242  # Höhe des Zimmers in cm
 
-# defininig cake size in easy units
-r_cm = 15       # radius, cm
-h_cm = 9        # hight, cm
-N = 9           # piece count, no unit
-rho_L = 0.3     # density, kg per L
-k_g = 350       # calories per mass, kcal per 100_g
-pi = 3.141592
+l = l_cm / 100     # Länge des Zimmers in m
+b = b_cm / 100     # Breite des Zimmers in m
+h = h_cm / 100     # Höhe des Zimmers in m
 
-# converting the units to SI
-r = r_cm / 100      # cm -> m
-h = h_cm / 100      # cm -> m
-rho = rho_L * 1000  # kg per L -> kg per m^3
-k = k_g * 10        # kcal per 100g -> kcal per kg
+e = 5       # Quadratmeterzahl pro Liter Farbe in cm²/L
 
+Literpreis = 4.99   # Literpreis der Farbe in €/L
 
-piece_v = (r**2 * pi * h) / N
-piece_v_cm3 = piece_v * 1000 *1000  # m3 to cm3, since it is a more useful unit in that case
+Flaeche = (2 * l * h + 2 * b * h + l * b)
+Volumen_Farbe = Flaeche / e
+Gesamtpreis = Volumen_Farbe * Literpreis
 
-piece_cal = piece_v * rho * k
-
-print('Alle Kuchenstücke sind ' + str(round(piece_v_cm3, 2)) + ' cm3 groß und haben ' + str(round(piece_cal,2)) + ' kcal!')
-
-print('ungerundete Werte: ')
-print('  1. Volumen: ' + str(piece_v_cm3))
-print('  2. Energie: ' + str(piece_cal))
-
-
-
+print(f"Sie werden eine Fläche von ca. {Flaeche} m² streichen. \n"
+      f"Dafür benötigen Sie {round(Volumen_Farbe, 3)}L Farbe. \n"
+      f"Diese kostet {round(Gesamtpreis, 3)}€.")
 
 # -------------
 # Aufgabe 3
 # -------------
 
 print('\n\n############### Aufgabe 3')
-print('Willkommen im Expenditure Compliance Support Client.')
+print('Compliance Check für deine Fahrt -- Cheap Transport Ltd.')
+
 
 # um den Input bitten
-budget = int(input('Wie groß ist das Budget? > '))
-price = int(input('Was kostet das was du anschaffen willst? > '))
+planned_load  = int(input('Was ist das Gewicht der geplanten Ladung? > '))
+max_load = int(input('Was ist die maximal zulässige Zuladung des Fahrzeugs? > '))
 
 # Ausgaben identifizieren (1.)
 # TODO: Zeilen markieren in denen etwas ausgegeben wird.
 
-if 0 < price <= 200:
-    print('Das liegt in deiner Verantwortung.')     #Ausgabe
-elif price > 200:
-    if 10_000 > budget > price:
-        print('Es braucht die Unterschrift einer Budgetverantwortlichen!')  #Ausgabe
-    elif budget < 10_000 and budget * 1.1 > price:
-        print('Es braucht die Unterschrift zweier Budgetverantwortlichen!')     #Ausgabe
-    elif price > budget:
-        print('Das bleibt wohl eher ein Traum')     #Ausgabe
-elif budget > 10_000:
-    print('Das muss in einem Ausschuss diskutiert werden!') #Ausgabe
+if planned_load >= 20 and 0 < max_load < 7500:
+    if planned_load > max_load:
+        print('Nicht gut - aber geht noch!')    # Ausgabe
+    elif planned_load > max_load * 2:
+        print('Das ist jetzt sogar für uns zu viel.')   # unerreichba, da wenn erreicht, voerheriges auch immer erreicht
+    elif planned_load == max_load:
+        print('Punktlandung')   # Ausgabe
+    else:
+        print('Mehr Ladung möglich!')   # Ausgabe
 else:
-    print('Das ist wohl eher ein Fall für die Sales-Abteilung!')    #Ausgabe
+    if planned_load < 20:
+        print('Nur 20kg? Brings mim Fahrrad!')
+    if max_load >= 7500:
+        print('Dafür haben wir keinen Führerschein!')
+
 
 
 # Codeüberdeckung (2.)
-# TODO: Geben Sie hier die geforderten Inputs mit der jeweils erwarteten Ausgabe an
+# planned_load = 21
+# max_load = 1
+# Erzeugte Ausgabe: Nicht gut - aber geht noch!
 
-# price = 50
-# budget = 1000
-# Erzeugte Ausgabe: Das liegt in deiner Verantwortung.
+# planned_load = 21
+# max_load = 21
+# Erzeugte Ausgabe: Punktlandung
 
-# price = 201
-# budget = 1000
-# Erzeugte Ausgabe: Es braucht die Unterschrift einer Budgetverantwortlichen!
+# planned_load = 5
+# max_load = 5000
+# Erzeugte Ausgabe: Nur 20kg? Brings mim Fahrrad!
 
-# price = 201
-# budget = 190
-# Erzeugte Ausgabe: Es braucht die Unterschrift zweier Budgetverantwortlichen!
+# planned_load = 5
+# max_load = 8000
+# Erzeugte Ausgabe: Nur 20kg? Brings mim Fahrrad! und Dafür haben wir keinen Führerschein!
 
-# price = 201
-# budget = 100
-# Erzeugte Ausgabe: Das bleibt wohl eher ein Traum
+# planned_load = 21
+# max_load = 8000
+# Erzeugte Ausgabe: Dafür haben wir keinen Führerschein!
 
-# price = -10
-# budget = 10001
-# Erzeugte Ausgabe: Das muss in einem Ausschuss diskutiert werden!
+# planned_load = 20
+# max_load = 20
+# Erzeugte Ausgabe: Punktlandung
 
-# price = -10
-# budget = 1000
-# Erzeugte Ausgabe: Das ist wohl eher ein Fall für die Sales-Abteilung!
+# planned_load = TODO
+# max_load = TODO
+# Erzeugte Ausgabe: TODO
 
-# price = 201
-# budget = 10001
-# Erzeugte Ausgabe: $ kein output $
-
-# Anzahl von "Wegen" durch die Struktur: 7
-
-# Existiert ein "Weg" ohne Ausgabe? ja
+# Existiert ein "Weg" ohne Ausgabe? nein
 
 
 # -----------------------
 # Aufgabe 4 (Variante 1)
 # -----------------------
+print('Japanische Stockwerke')
+print(
+    f"In Japan werden Stockwerke mit einer Zahl und einem Buchstaben gekennzeichnet. \n"
+    f"Die Zahl gibt die Stockwerksnummer vom Erdgeschoss an. \n"
+    f"Der Buchstabe kennzeichnet ob das Stockwerk ober (F) oder unter (B) dem Erdgeschoss liegt. \n"
+    f"Die Zahl 4 wird meist ausgelassen. \n \n"
+    f"Dieses Programm konvertiert ein japanisches Stockwerk zu einem österreichischen Stockwerk. \n"
+)
 
-print('\n\n############### Aufgabe 4')  # NICHT Teil der Ausgabe die in einem einzigen print-Statement erzeugt werden soll.
-longitude = int(input('Längengrad (Longitude): '))
-latitude = int(input('Breitengrad (Latitude): '))
+choice = str(input("Geben Sie ein japanisches Stockwerk ein:"))
+
+digit = int(choice[0:len(choice) - 1])
+letter = str(choice[-1])
+
+oe_vz = ""
+oe_nummer = 0
+
+if digit < 5:
+    oe_nummer = digit - 1
+elif digit >= 5:
+    oe_nummer = digit - 2
+else:
+    print('Das sollte nicht passieren.')
+
+if letter == "B":
+    oe_vz = "-"
+    oe_nummer = oe_nummer + 1
 
 
-if -180 <= longitude <= 180 and -90 <= latitude <= 90:  # (1)
-    if latitude > 66 or latitude < -66:                 # (2)
-        x = 'Es ist Brrr .... kalt! '
-        if latitude > 66 :
-            print(x + '(Untergrund: Eis)')
-        elif latitude < -66:
-            print(x + '(Untergrund: Land)')
-    elif -23 < latitude < 23:                           # (3)
-        x = 'Ohhh ... warm!'
-        if latitude == 0:
-            y = 'Es ist immer gleich. '
-            if -80 <= longitude <= -35 or 10 <= longitude <= 45:
-                print(y + '(Untergrund: Land)')
-            elif 100 <= longitude <= 130:
-                print(y + '(Untergrund: Gemischt)')
-            else:
-                print(y + '(Untergrund: Wasser)')
-        else:
-            print(x)
-    else:
-        print('Es ist Gemäßigt ... I guess.')
-else:                                                   # (4)
-    print('Out of this world!')
+if digit == 4:
+    print(f"Ein japanisches Stockwerk mit der Zahl 4 wird meist ausgelassen. \n"
+          f"Sollte es doch vorkommen entspricht es dem {oe_vz}{oe_nummer}.Stock.")
+else:
+    print(f"Das Stockwerk {choice} entspricht dem {oe_vz}{oe_nummer}.Stock.")
